39a18072e2d9cb00d505a41eda22af06bc0bb0ed
Dmitry Zaets <dmitry.zaets@gmail.com>
2016-02-23 16:38:21 +0100

    Fix Indentation

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 static Window *window;
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  game_tick();
+	game_tick();
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, blue_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, red_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, green_click_handler);
+	window_single_click_subscribe(BUTTON_ID_SELECT, blue_click_handler);
+	window_single_click_subscribe(BUTTON_ID_UP, red_click_handler);
+	window_single_click_subscribe(BUTTON_ID_DOWN, green_click_handler);
 }
 
 static void window_load(Window *window) {
@@ -18,26 +18,26 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
-  clean_up();
+	clean_up();
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
+	window = window_create();
+	window_set_click_config_provider(window, click_config_provider);
+	window_set_window_handlers(window, (WindowHandlers) {
 		.load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+			.unload = window_unload,
+	});
+	const bool animated = true;
+	window_stack_push(window, animated);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+	window_destroy(window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+	init();
+	app_event_loop();
+	deinit();
 }
