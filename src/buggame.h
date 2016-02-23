93c307a4b6d469ffe5cbbefd3a6f35a94e6a9023
Oleksandr Chernenko <oleks.chernenko@gmail.com>
2016-02-23 16:11:23 +0100

    Initial commit

---
+++ src/buggame.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <pebble.h>
+
+enum BugType { Red = 0, Blue = 1, Green = 2 };
+
+enum Direction { Up, Down, Left, Right };
+
+typedef struct {
+	 bool isPlayer;
+	 bool isProcessed;
+	 enum BugType type;
+} Bug;
+
+// button handlers
+void blue_click_handler(ClickRecognizerRef recognizer, void *context);
+void red_click_handler(ClickRecognizerRef recognizer, void *context);
+void green_click_handler(ClickRecognizerRef recognizer, void *context);
+
+// start, tick and stop
+void warm_up(Window *window);
+void game_tick();
+void clean_up();
